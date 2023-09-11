1825684be6fff7f072fd0d0b596ded63cb3714dc
Zagal12 <144718646+Zagal12@users.noreply.github.com>
2023-09-11 20:39:57 +0300

    Add files via upload

---
+++ form-hndler.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_post['name'];
+$visitor_email = $_post['email'];
+$subject = $_post['subject'];
+$message= $_post['message'];
+
+$email_form = 'info@yourwebsite.com';
+
+$email_subject = 'new form submission';
+
+$email_body = "user name: $name.\n"
+              "user Email: $visitor_email.\n"
+              "subject: $subject.\n"
+              "user Message: $message.\n";
+
+$to = 'randasagal@gmail.com';
+
+$headers = "form: $email_form \r\n";
+
+$headers .="Reply_to: $visitor_email\r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location. cuntact.html.html");
+?>
